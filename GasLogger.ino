d2e83171dd416ae89e9ae64323b958d5c4a760a9
Duncan Byrne <byrne@mines.edu>
2023-10-22 19:44:40 -0600

    Added CO2 sensor initalization

--- GasLogger.ino
+++ GasLogger.ino
@@ -1,5 +1,12 @@
+#include <SoftwareSerial.h>
+
 #include "DFRobot_SD3031.h" // package for RTC
 #include "DFRobot_OxygenSensor.h" // package for O2 Sensor
+
+// init UART for CO2 sensor
+SoftwareSerial mySerial(10, 11); // RX TX
+// hex data to retrieve a measurement from CO2 Sensor
+unsigned char hexData[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79};
 
 // init sensor/module classes
 DFRobot_SD3031 rtc;
@@ -12,6 +19,11 @@ DFRobot_OxygenSensor oxygen;
 void setup() {
   // init serial
   Serial.begin(9600);
+  // begin serial connection with CO2 Sensor
+  while (!Serial) {
+  }
+  mySerial.begin(9600);
+  Serial.println("CO2 sensor successfully initalized");
   // test & init RTC
   while (rtc.begin() != 0) {
     Serial.println("Failed to initalize RTC");
@@ -22,6 +34,8 @@ void setup() {
     Serial.println("Failed to initalize O2 sensor");
   }
   Serial.println("O2 sensor successfully initalized");
+  
+  
 }
 
 void loop() {
