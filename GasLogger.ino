06fc3c969e02d5d74e00a94ab03f83e125fcdc8b
Duncan Byrne <byrne@mines.edu>
2023-10-22 20:33:30 -0600

    Added RTC time setting & CO2 sensor

--- GasLogger.ino
+++ GasLogger.ino
@@ -28,14 +28,99 @@ void setup() {
   while (rtc.begin() != 0) {
     Serial.println("Failed to initalize RTC");
   }
+  rtc.setHourSystem(rtc.e24hours);
   Serial.println("RTC successfully initalized");
   // test & init O2 sensor
   while (!oxygen.begin(oxygen_addr)) {
     Serial.println("Failed to initalize O2 sensor");
   }
   Serial.println("O2 sensor successfully initalized");
+}
+
+sTimeData_t sTime;
+void getTime() {
+  sTime = rtc.getRTCTime();
+  Serial.print(sTime.year, DEC);//year
+  Serial.print('/');
+  Serial.print(sTime.month, DEC);//month
+  Serial.print('/');
+  Serial.print(sTime.day, DEC);//day
+  Serial.print(' ');
+  Serial.print(sTime.hour, DEC);//hour
+  Serial.print(':');
+  Serial.print(sTime.minute, DEC);//minute
+  Serial.print(':');
+  Serial.print(sTime.second, DEC);//second
+  Serial.println(' ');
+}
+
+int getSec() {
+  sTime = rtc.getRTCTime();
+  return sTime.second;
+}
+
+int pSec = -1;
+bool timeSet = false;
+byte year;
+byte month;
+byte date;
+byte dOW;
+byte hour;
+byte minute;
+byte second;
+void setTime(byte& year, byte& month, byte& date, byte& dOW,
+                  byte& hour, byte& minute, byte& second) {
+  // Call this if you notice something coming in on
+  // the serial port. The stuff coming in should be in
+  // the order YYMMDDwHHMMSSx
+  boolean gotString = false;
+  timeSet = true;
+  char inChar;
+  byte temp1, temp2;
+  char inString[20];
   
-  
+  byte j=0;
+  while (!gotString) {
+    if (Serial.available()) {
+      inChar = Serial.read();
+      inString[j] = inChar;
+      j += 1;
+      if (inChar == 'x') {
+        gotString = true;
+      }
+    }
+    //if (getSec > pSec+10) { break; }
+  }
+  Serial.println(inString);
+  // Read year first
+  temp1 = (byte)inString[0] -48;
+  temp2 = (byte)inString[1] -48;
+  year = temp1*10 + temp2;
+  // now month
+  temp1 = (byte)inString[2] -48;
+  temp2 = (byte)inString[3] -48;
+  month = temp1*10 + temp2;
+  // now date
+  temp1 = (byte)inString[4] -48;
+  temp2 = (byte)inString[5] -48;
+  date = temp1*10 + temp2;
+  // now Day of Week
+  dOW = (byte)inString[6] - 48;
+  // now hour
+  temp1 = (byte)inString[7] -48;
+  temp2 = (byte)inString[8] -48;
+  hour = temp1*10 + temp2;
+  // now minute
+  temp1 = (byte)inString[9] -48;
+  temp2 = (byte)inString[10] -48;
+  minute = temp1*10 + temp2;
+  // now second
+  temp1 = (byte)inString[11] -48;
+  temp2 = (byte)inString[12] -48;
+  second = temp1*10 + temp2;
+  rtc.setTime(2000+year,month,date,hour,minute,second);//Initialize time
+  Serial.print("Sucessfully set time to: ");
+  getTime();
 }
 
 long hi, lo, CO2;
@@ -58,7 +143,22 @@ int measureCO2() {
   return CO2;
 }
 
+float O2;
+float measureO2() {
+  O2 = oxygen.getOxygenData(collect_num);
+  return O2;
+}
+
 void loop() {
-  Serial.println(measureCO2());
-  delay(1000);
+  if (pSec != getSec()) {
+    Serial.print(measureCO2());
+    Serial.print(" | ");
+    Serial.println(measureO2());
+    getTime();
+    pSec = getSec();
+  }
+  //check to see if time needs to be updated
+  if (Serial.available() && (timeSet == false)) {
+    setTime(year, month, date, dOW, hour, minute, second);
+  }
 }
