122dcb660d7a4cd5e9ab765c93f1a3342f672e44
Duncan Byrne <byrne@mines.edu>
2023-11-12 14:07:24 -0700

    Completed CO2 Callibration code.

--- CallibrateCO2Sensor/CallibrateCO2Sensor.ino
+++ CallibrateCO2Sensor/CallibrateCO2Sensor.ino
@@ -4,11 +4,34 @@
 SoftwareSerial mySerial(8, 9); // RX TX
 // hex data to retrieve a measurement from CO2 Sensor
 unsigned char hexData[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79};
+// hex to disable self callibration
+unsigned char disSelfCal[9] = {0xFF, 0x01, 0x79, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80};
+// hex for zero point 
+unsigned char setZero[9] = {0xFF, 0x01, 0x87, 0x00, 0x00, 0x00, 0x00, 0x00, 0x78};
 
 void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
   Serial.println(F("CO2 sensor successfully initalized"));
+  Serial.println(F("Disabling self callibration"));
+  mySerial.write(disSelfCal, 9);
+  Serial.println(F("Waiting 20 minutes before setting zero point to 400ppm"));
+  Serial.println(F("During this period ensure the device is in a stable 400ppm environment"));
+  float percent;
+  for (int i = 0; i < 60*20; i++) {
+    // Move the cursor to the beginning of the line
+    Serial.print("\r");
+    Serial.print(i);
+    Serial.print("s waited out of 1200, or ");
+    percent = 100.0*float(i)/1200.0;
+    Serial.print(percent);
+    Serial.println("%                        ");
+    Serial.flush();
+    delay(1000);
+  }
+  Serial.println(F("Performing zero callibration"));
+  mySerial.write(setZero, 9);
+  Serial.println(F("Successfully performed zero callibration"));
 }
 
 long hi, lo, CO2;
